d26487362a8834f3832881409cc2bd861fe038fa
Junaidi Saami (XC-CE/ENG3-AU) <jus1cl@bosch.com>
2025-07-17 16:37:18 +1000

    fix: made changes for main.py to pass python workflow part 3

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import re
 
 def set_github_action_output(output_name, output_value):
     with open(os.environ["GITHUB_OUTPUT"], "a") as f:
-        f.write(f'{output_name}={output_value}\n')  
+        f.write(f'{output_name}={output_value}\n')
 
 
 def main():
