37ad7e4d5af9998cf5ae1f46cf6827bd9c23ebce
Junaidi Saami (XC-CE/ENG3-AU) <jus1cl@bosch.com>
2025-07-17 16:17:31 +1000

    Created an action that validates commit messages absed on a standardised approach where they must begin with either 'feat', 'fix' or 'docs'

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
+import re
 
-# Set the output value by writing to the outputs in the Environment File, mimicking the behavior defined here:
-#  https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
 def set_github_action_output(output_name, output_value):
     f = open(os.path.abspath(os.environ["GITHUB_OUTPUT"]), "a")
     f.write(f'{output_name}={output_value}')
     f.close()    
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
+    commit_message = os.environ.get("INPUT_COMMITMESSAGE", "").strip()
+    print(f"Commit message: '{commit_message}'")
 
-    my_output = f'Hello {my_input}'
+    pattern = r"^(feat|fix|docs): .+"
+    is_valid = bool(re.match(pattern, commit_message))
 
-    set_github_action_output('myOutput', my_output)
-
+    if is_valid:
+        set_github_action_output("outcome", "valid")
+    else:
+        set_github_action_output("outcome", "invalid")
 
 if __name__ == "__main__":
     main()
